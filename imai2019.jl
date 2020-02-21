85a52b92ad1a06d3af3ef16eec9a3f9231f34265
takuizum <sep10.taku.izum@gmail.com>
2020-02-21 22:28:39 +0900

    Revert "Analyze by using julia"
    This reverts commit e8173e4aaafbbc7e6111a1441cb87eb3eec0a6cb.

--- imai2019.jl
+++ imai2019.jl
@@ -1,17 +1,19 @@
 
 using Pkg
-Pkg.add(["CSV", "SpecialFunctions", "RCall"])
+Pkg.add("CSV")
+Pkg.add("SpecialFunctions")
 
-using LinearAlgebra, SpecialFunctions, RCall
+using CSV
+using LinearAlgebra
+using SpecialFunctions
 
-R"""
-# install.packages("RcppSMC")
-data(radiata,package = "RcppSMC")
-n <- nrow(radiata)
-X <- cbind(rep(1,n),radiata$x1)
-y <- matrix(radiata$y,ncol=1)
-"""
-@rget n X y
+
+X = CSV.read("C:/Users/dhojo/Desktop/X.csv")
+y = CSV.read("C:/Users/dhojo/Desktop/y.csv")
+n = size(X)[1]
+
+X = convert(Matrix,X)
+y = convert(Matrix,y)
 
 r_0 = 0.06
 s_0 = 6
@@ -31,46 +33,8 @@ logML = -(n/2)*log(pi) + (a_0/2)*log(b_0) + log( gamma((n+a_0)/2)/gamma(a_0/2) )
   (1/2)*log( det(Q_0)/det(M) ) - ((n+a_0)/2)*log((y'*R*y)[1,1]+b_0)
 logML
 
-## Stan
-using CmdStan
-Imai2019_stan = open("Imai2019_model1.stan", "r") do f
-    readlines(f, keep = true)
-end |> join
-mod1 = Stanmodel(name = "imai2019"; model = Imai2019_stan,
-                 num_samples = 1000, num_warmup = 1000, thin = 1, nchains = 4, random = CmdStan.Random(1234))
-display(mod1)
 
-data1 = Dict("n" => n,
-             "x" => X[:,2],
-             "y" => y[:],
-             "mode" => 1 # WBIC
-             )
 
-rc, smpl, cname = stan(mod1, data1)
-display(cname)
 
-using Statistics
-occursin.(r"log_lik", cname)
-sum(x -> occursin(r"log_lik", x), cname)
-function wbic(smpl, cname)
-    n = sum(x -> occursin(r"log_lik", x), cname)
-    loc = findall(occursin.(r"log_lik", cname))
-    log_lik_smpl = smpl[:,loc,:]
-    sum(log_lik_smpl; dims = 2) |> mean
-end
 
-wbic(smpl, cname) |> display
-# -308.65735830750003
-## WBIC - v_t
 
-function gf_variance(smpl, cname)
-    n = sum(x -> occursin(r"log_lik", x), cname)
-    loc = findall(occursin.(r"log_lik", cname))
-    log_lik_smpl = smpl[:,loc,:]
-    log_lik = sum(log_lik_smpl; dims = 2)
-    mean(mean(log_lik_smpl .^2; dims = 2) .- mean(log_lik_smpl; dims = 2) .^2)
-end
-gf_variance(smpl, cname)
-
-v_t = 0.5log(n) * gf_variance(smpl, cname)
-wbic(smpl, cname) - v_t |> display
